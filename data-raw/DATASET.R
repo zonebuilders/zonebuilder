d4483f7987ef014daae0f5a6653ee9d69c41dc5b
Robin Lovelace <rob00x@gmail.com>
2019-12-14 22:44:35 +0000

    Add zb_region_midpoint

---
+++ data-raw/DATASET.R
@@ -0,0 +1,23 @@
+## code to prepare the datasets used in this package
+
+library(sf)
+library(tidyverse)
+
+zb_region = spData::lnd %>% 
+  st_transform(27700) %>% 
+  st_union()
+
+# identical(zb_region, zb_region2)
+
+usethis::use_data(zb_region)
+
+zb_region_midpoint = tmaptools::geocode_OSM("london uk", as.sf = TRUE) %>% 
+  st_transform(27700)
+
+zb_region_centroid = st_centroid(zb_region)
+plot(zb_region)
+plot(zb_region_midpoint$geometry, add = TRUE)
+plot(zb_region_centroid, add = TRUE, col = "red")
+
+usethis::use_data(zb_region_midpoint)
+
