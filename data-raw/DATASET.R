ea233a862d03a6d50a1c0452e7718994716d13c7
Robin Lovelace <rob00x@gmail.com>
2019-12-14 22:46:33 +0000

    Rename file

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -11,13 +11,13 @@ zb_region = spData::lnd %>%
 
 usethis::use_data(zb_region)
 
-zb_region_midpoint = tmaptools::geocode_OSM("london uk", as.sf = TRUE) %>% 
+zb_region_cent = tmaptools::geocode_OSM("london uk", as.sf = TRUE) %>% 
   st_transform(27700)
 
 zb_region_centroid = st_centroid(zb_region)
 plot(zb_region)
-plot(zb_region_midpoint$geometry, add = TRUE)
+plot(zb_region_cent$geometry, add = TRUE)
 plot(zb_region_centroid, add = TRUE, col = "red")
 
-usethis::use_data(zb_region_midpoint)
+usethis::use_data(zb_region_cent)
 
